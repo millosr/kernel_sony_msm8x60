778aa8ecf5093cc379615047e614cb2a3a2639c9
Milos Ratkovic <milosr@gmail.com>
2018-08-30 23:42:17 +0200

    pie: initial changes

--- Android.mk
+++ Android.mk
@@ -4,8 +4,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-KERNEL_DIR := $(ANDROID_BUILD_TOP)/kernel/msm
-KERNEL_TOOLS_PREFIX := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-4.7/bin/arm-eabi-
+KERNEL_DIR := ./kernel/msm
+KERNEL_TOOLS_PREFIX := /hdd1/work/aosp-9/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-4.7/bin/arm-eabi-
 -include $(KERNEL_DIR)/AndroidKernel.mk
 
 LOCAL_PREBUILT_MODULE_FILE := $(TARGET_PREBUILT_KERNEL)
